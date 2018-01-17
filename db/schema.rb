9ae36f17d99df0dbcd57337d7c1c4c50faf8d569
laibuc <ljyuan5193@gmail.com>
2018-01-17 21:11:58 +0800

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 20180117125541) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
